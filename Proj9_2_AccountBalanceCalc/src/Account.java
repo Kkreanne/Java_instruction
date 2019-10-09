3e77e267e27acb777af437b68a7bdfc7c88ca531
Kkreanne <54691061+Kkreanne@users.noreply.github.com>
2019-10-09 07:27:55 -0400

    9-2 Account Balance Calculator

---
+++ Proj9_2_AccountBalanceCalc/src/Account.java
@@ -0,0 +1,35 @@
+
+public class Account implements Depositable, Withdrawable, Balancable {
+	
+	protected double balance;
+
+	public Account() {
+		super();
+	}
+
+	public Account(double balance) {
+		super();
+		this.balance = balance;
+	}
+
+	@Override
+	public double getBalance() {
+		return balance;
+	}
+
+	@Override
+	public void setBalance(double amount) {
+		balance = amount;
+	}
+
+	@Override
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+
+	@Override
+	public void deposit(double amount) {
+		balance += amount;
+	}
+
+}
